b0a9d53d620abf00bc09110d6cfc558c419cb021
mutenesss <erickfraga0301@gmail.com>
2026-07-12 13:17:59 -0300

    Adiciona processamento de multiplas imagens em uma pasta

--- main/main.py
+++ main/main.py
@@ -6,7 +6,13 @@ from pathlib import Path
 start = Path.cwd().parent
 img_path = start / "images"
 img_output_path = img_path / "output"
-
+"""
+subdirs = [x for x in img_path.iterdir() if x.is_dir() and x != img_output_path]
+fileList = [x for x in img_path.iterdir() if x.is_file()]
+for x in fileList:
+    print(x.name)
+print(subdirs)
+"""
 """
 Retira pixeis da imagem para manter a mesma com tamanho multiplo de 8
 INPUT:
@@ -15,12 +21,10 @@ OUTPUT:
     result - imagem apos retirada de pixeis
 """
 def resize_img(img: np.ndarray) -> np.ndarray:
-    h,w,_ = img.shape
-    h_pad = h%8
-    w_pad = w%8
-    h-=h_pad
-    w-=w_pad
-    result = img[:h, :w]
+    h,w = img.shape[:2]
+    h8 = h - (h%8)
+    w8 = w - (w%8)
+    result = img[:h8, :w8]
     return result
 
 """
@@ -39,7 +43,7 @@ def jpeg_cycle(img: np.ndarray, quality: int) -> np.ndarray:
         decoded = cv2.imdecode(rmem, 1)
         return decoded
     else:
-        raise RuntimeError("Incapaz de executar ciclo de compressao e descompressao JPEG.")
+        raise RuntimeError("Incapaz de executar ciclo de compressao e descompressao JPEG.\n")
 
 """
 Aplica n ciclos JPEG em uma imagem ate atingir o ponto fixo ou chegar ao maximo de iteracoes.
@@ -53,19 +57,20 @@ OUTPUT:
     OU
     current_img - imagem em seu ponto fixo apos i iteracoes
 """
-def create_tamper_evident(img: np.ndarray, quality: int, max_iters: int = 200, tol: int = 0) -> np.ndarray:
+def create_tamper_evident(img: np.ndarray, quality: int, max_iters: int = 100, tol: int = 0) -> (np.ndarray | None):
     current_img = img.copy()
     for i in range(max_iters):
         next_img = jpeg_cycle(current_img, quality)
-        img_dif = np.abs(next_img.astype(np.uint8) - current_img.astype(np.uint8))
+        img_dif = np.abs(next_img.astype(np.uint16) - current_img.astype(np.uint16))
         num_dif = np.any(img_dif > tol, axis=-1).sum()
+        del img_dif
         print(f"Iteracao atual: {i}, Pixeis alterados: {num_dif}")
         if(num_dif == 0):
             print(f'Ponto fixo atingido em i={i}.')
             return current_img
         current_img = next_img
-    raise RuntimeWarning("Imagem nao convergiu dentro do numero de iteracoes")
-    return current_img
+    print("Imagem nao convergiu dentro do numero de iteracoes\n")
+    return None
 
 """
 Calcula a variacao entre os blocos JPEG de uma imagem apos a aplicacao de um ciclo JPEG
@@ -86,24 +91,28 @@ def check_tamper(img: np.ndarray, quality: int):
     img_tamper = jpeg_cycle(img_arr, quality).astype(np.uint8)
 
     diff_array = np.any(np.abs(img_arr-img_tamper) > 0, axis=-1)
+    del img_arr
+    del img_tamper
     mask_array = np.zeros((h//8, w//8), dtype=bool)
-
-    for block_y in range(mask_array.shape[0]):
-        for block_x in range(mask_array.shape[1]):
-            block = diff_array[block_y*8:(block_y+1)*8,
-                               block_x*8:(block_x+1)*8]
-            mask_array[block_y, block_x] = block.any()
+    mask_array = diff_array.reshape(h//8, 8, w//8, 8). any(axis=(1,3))
     return mask_array, diff_array
 
 """
+Gera a imagem com alteracoes caso existam
+INPUT:
+    img - imagem considerada alterada
+    mask - mascara de alteracoes computada por check_tamper
+OUTPUT:
 """
-def show_diff(img: np.ndarray, mask: np.ndarray) -> None:
+def show_diff(img: np.ndarray, mask: np.ndarray, outputPath: Path) -> None:
     if mask.sum() != 0:
         h = mask.shape[0]*8
         w = mask.shape[1]*8
 
         img_crop = img[:h,:w].copy()
         red = np.zeros((h,w),dtype=bool)
+        del h
+        del w
 
         for block_y in range(mask.shape[0]):
             for block_x in range(mask.shape[1]):
@@ -114,40 +123,72 @@ def show_diff(img: np.ndarray, mask: np.ndarray) -> None:
         red_layer = np.zeros_like(img_crop)
         red_layer[:, :] = [0,0,255]
         
-        blended = cv2.addWeighted(img_crop, 1-0.5, red_layer, 0.5, 0)
+        blended = cv2.addWeighted(img_crop, 0.5, red_layer, 0.5, 0)
         result = img_crop.copy()
+        del img_crop
         result[red] = blended[red]
 
-        print(f'Quantidade de blocos alterados: {mask.sum()/mask.size}')
-        cv2.imwrite(img_output_path/"changed.jpg", result)
+        print(f'Porcentagem de blocos alterados: {mask.sum()/mask.size}')
+        fileName = (img_output_path/outputPath.stem)
+        fileName = fileName.with_name(fileName.stem + "_changed.jpg")
+        cv2.imwrite(fileName, result)
+        del result
+        print("Detectada alteracao na imagem.\n")
     else:
-        print("Nao foi detectada nenhuma alteracao na imagem")
+        print("Nao foi detectada nenhuma alteracao na imagem.\n")
 
+
+subdirs = [x for x in img_path.iterdir() if x.is_dir() and x != img_output_path]
+subdirs.append(img_path)
+quality = 90
+fileList = [x for x in img_path.iterdir() if x.is_file()]
+failed = []
+
+for dirs in subdirs:
+    fileList = [x for x in dirs.iterdir() if x.is_file]
+    for file in fileList:
+        if file.suffix:
+            print(f"Testando arquivo: {file.name}")
+            print(f"Diretorio: {dirs}\n")
+            cur_img = cv2.imread(file)
+
+            crop_img = resize_img(img=cur_img)
+            del cur_img
+            tamper_img = create_tamper_evident(img=crop_img, quality=quality)
+            if tamper_img is not None:
+                del crop_img
+                tamper_img = resize_img(img=tamper_img)
+
+                output_name = (img_output_path/file.stem)
+                output_name = output_name.with_name(output_name.stem + "_tamper.jpg")
+                cv2.imwrite(output_name, tamper_img, [cv2.IMWRITE_JPEG_QUALITY, quality])
+                del tamper_img
+
+                test_img = cv2.imread(output_name)
+                change_img = cv2.blur(test_img, (50,50))
+                mask, diff = check_tamper(img=change_img, quality=quality)
+                del change_img
+                #mask, diff = check_tamper(img=test_img, quality=quality)
+                print(f"Quantidade de blocos alterados: {mask.sum()} de {mask.size}\n")
+                show_diff(img=test_img, mask=mask, outputPath=output_name)
+                del test_img
+            else:
+                failed.append(file.name)
+
+print("Os seguintes arquivos falharam em convergir:\n")
+for fail in failed:
+    print(fail)
+
+"""
 quality = 90
 img = cv2.imread(img_path/"original_organic_1.jpg")
-proc_img = resize_img(img=img)
-te_img = create_tamper_evident(img=proc_img, quality=quality)
+crop_img = resize_img(img=img)
+te_img = create_tamper_evident(img=crop_img, quality=quality)
+te_img = resize_img(te_img)
 cv2.imwrite(img_output_path/"tamper_evident.jpg",te_img, [cv2.IMWRITE_JPEG_QUALITY, quality])
 
 sus_img = cv2.imread(img_output_path/"tamper_evident.jpg")
 mask, diff = check_tamper(img=sus_img, quality=quality)
 print(f"Quantidade de blocos alterados: {mask.sum()} de {mask.size}")
 show_diff(img=sus_img, mask=mask)
-
-"""
-test_img = cv2.imread(img_path/"face.png",1)
-diff_array = []
-if test_img is not None:
-    cv2.imwrite(img_output_path/"image.jpg",test_img,[cv2.IMWRITE_JPEG_QUALITY, 10])
-    new_img = cv2.imread(img_output_path/"image.jpg",1)
-    dif = (test_img != new_img).sum()
-    print(dif)
-    diff_array.append(dif)
-    while(dif != 0 ):
-        cv2.imwrite(img_output_path/"image.jpg",test_img,[cv2.IMWRITE_JPEG_QUALITY, 10])
-        prev_img = new_img
-        new_img = cv2.imread(img_output_path/"image.jpg",1)
-        dif = (prev_img != new_img).sum()
-        diff_array.append(dif)
-        print(dif)
 """
